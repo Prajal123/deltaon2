fb0d1aaefcdcc1bfcab255e3ca56dfaf62d62334
Prajal123 <106120085@nitt.edu>
2021-08-25 17:10:15 +0530

    Updated config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ $servername="localhost";
 $username="root";
 $password="";
 $database="url shortener";
-$base_url="https://localhost/deltaon/";
+$base_url="https://localhost/deltaon1/";
 $conn=mysqli_connect($servername,$username,$password,$database);
 if(!$conn){
     echo "Something went wrong";
